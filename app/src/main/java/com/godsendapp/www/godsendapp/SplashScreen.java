2e299ddb8fe05893b8c65b9723eb606d537e490b
Jon Garrison <Jon Garrison>
2016-11-19 12:39:14 -0500

    Initial commit

---
+++ app/src/main/java/com/godsendapp/www/godsendapp/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.godsendapp.www.godsendapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Jon on 11/16/2016.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+}
